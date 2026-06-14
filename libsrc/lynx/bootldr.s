a3bf810c59308fd514a9a30c5d8a7a6db8135d40
Igor Kromin <igor@atarigamer.com>
2026-06-14 07:54:52 +0000

    lynx: enhance bootldr primary loader (palette clear + AUDIN/IODIR init)
    Port the enhanced primary loader from newbootldr.s: clears the palette
    and sets up AUDIN/IODIR (AUDIN driven low, ZP shadow at $1a) instead of
    just powering the ROM on. The encrypted/signed primary block is copied
    verbatim; plaintext comments updated to match. Secondary loader unchanged.
    Verified: all samples boot.

--- libsrc/lynx/bootldr.s
+++ libsrc/lynx/bootldr.s
@@ -25,15 +25,28 @@
 ;       ; 1. force Mikey to be in memory
 ;       stz MAPCTL
 ;
+;       ; 2. clear palette
+;       ldy #31
+;       lda #0
+;nextc: sta GCOLMAP, y
+;       dey
+;       bpl nextc
+;
 ;       ; 3. set ComLynx to open collector
 ;       lda #4          ; a = 00000100
 ;       sta SERCTL      ; set the ComLynx to open collector
 ;
-;       ; 4. make sure the ROM is powered on
-;       lda #8          ; a = 00001000
-;       sta IODAT       ; set the ROM power to on
+;       ; 4. set AUDIN to output
+;       lda #$1a        ; audin = out, rest = out, noexp = in,
+;                       ; cart addr = out, ext pwd = in
+;       sta IODIR
 ;
-;       ; 5. read in secondary exe + 8 bytes from the cart and store it in $f000
+;       ; 5. set AUDIN to LOW (also cart addr low for pwr to cart rom)
+;       lda #$08
+;       sta $1a         ; save local copy to ZP
+;       sta IODAT
+;
+;       ; 6. read in secondary exe + 8 bytes from the cart and store it in $f000
 ;       ldx #0          ; x = 0
 ;       ldy #$97        ; y = secondary loader size (151 bytes)
 ;rloop1: lda RCART0     ; read a byte from the cart
@@ -42,20 +55,20 @@
 ;       dey             ; y--
 ;       bne rloop1      ; loops until y wraps
 ;
-;       ; 6. jump to secondary loader
+;       ; 7. jump to secondary loader
 ;       jmp EXE         ; run the secondary loader
 ;
 ;       .reloc
 ;**********************************
 ; After compilation, encryption and obfuscation it turns into this.
 ;**********************************
-        .byte $ff, $81, $ca, $33, $be, $80, $a2, $c4 
-        .byte $6d, $98, $fe, $8d, $bc, $66, $c0, $7a 
-        .byte $09, $50, $23, $28, $18, $c8, $06, $70 
-        .byte $58, $4f, $1b, $e1, $c7, $90, $08, $cd 
-        .byte $1a, $6e, $5a, $45, $32, $d7, $6d, $c6 
-        .byte $8a, $e5, $d8, $5c, $a0, $e8, $4f, $7a 
-        .byte $5f, $73, $8d, $22
+        .byte $ff, $31, $cb, $62, $b4, $4f, $ee, $84
+        .byte $3b, $dc, $ab, $d3, $cb, $df, $e3, $77
+        .byte $e6, $35, $38, $2b, $52, $81, $d5, $c3
+        .byte $ce, $fd, $e0, $d3, $32, $68, $32, $a2
+        .byte $b3, $79, $36, $95, $fa, $77, $59, $80
+        .byte $12, $01, $25, $49, $d2, $e1, $6e, $5d
+        .byte $55, $63, $86, $18
 
 ;**********************************
 ; Now we have the secondary loader
